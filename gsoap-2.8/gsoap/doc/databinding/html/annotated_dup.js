8a4f6c4d6f668ebaeb9fa34ca8a2f809cd057a40
wwl33695 <wwl33695@126.com>
2019-01-14 16:35:19 +0800

    add gsoap

---
+++ gsoap-2.8/gsoap/doc/databinding/html/annotated_dup.js
@@ -0,0 +1,7 @@
+var annotated_dup =
+[
+    [ "std", "namespacestd.html", "namespacestd" ],
+    [ "_a__address_book", "class__a____address__book.html", "class__a____address__book" ],
+    [ "a__address", "classa____address.html", "classa____address" ],
+    [ "g", "classg.html", "classg" ]
+];
